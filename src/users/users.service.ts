9df854be59f91278ada793f903bce754523a030f
gkmit-dikshant <dikshant@gkmit.co>
2025-12-20 14:11:05 +0530

    fix: handle errors in services

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,25 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateUserTableDto } from './dto/create-user-table.dto';
 import { DataSource } from 'typeorm';
 import { fakeRow } from 'src/constants/allowed_columns';
 import { ALLOWED_DATATYPE } from 'src/constants/datatypes';
-
-function createQuery(id, table_name, schema) {
-  const table = `${table_name}_${id}`;
-  let c = '';
-
-  schema.forEach((element) => {
-    c += `${element.name} ${ALLOWED_DATATYPE[element.datatype]},`;
-  });
-
-  if (c.endsWith(',')) {
-    c = c.slice(0, -1);
-  }
-
-  const q = `CREATE TABLE ${table} ( ${c} );`;
-
-  return q;
-}
 
 @Injectable()
 export class UsersService {
@@ -33,88 +16,117 @@ export class UsersService {
   }
 
   async create(userId, createUserTableDto: CreateUserTableDto) {
-    const { tableName, schema } = createUserTableDto;
-    const q = createQuery(userId, tableName, schema);
+    try {
+      const { tableName, schema } = createUserTableDto;
 
-    const res: any = await this.dataSource.query(q);
+      const table = `${tableName}_${userId}`;
+      let c = '';
 
-    return {
-      message: 'success',
-      userId,
-    };
+      schema.forEach((element) => {
+        c += `${element.name} ${ALLOWED_DATATYPE[element.datatype]},`;
+      });
+
+      if (c.endsWith(',')) {
+        c = c.slice(0, -1);
+      }
+
+      const q = `CREATE TABLE ${table} ( ${c} );`;
+      const res: any = await this.dataSource.query(q);
+
+      return {
+        message: 'success',
+      };
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.UNPROCESSABLE_ENTITY);
+    }
   }
 
   async seedData(userId: string, tableName: string, rows: number) {
-    const _tableName = `${tableName}_${userId}`;
+    try {
+      const _tableName = `${tableName}_${userId}`;
 
-    const q = `
-    SELECT column_name
-    FROM information_schema.columns
-    WHERE table_name = '${_tableName}';
-  `;
+      const q = `
+      SELECT column_name
+      FROM information_schema.columns
+      WHERE table_name = '${_tableName}';
+      `;
 
-    const res: any[] = await this.dataSource.query(q);
-    let t = '';
+      const res: any[] = await this.dataSource.query(q);
+      let t = '';
 
-    for (let i = 1; i <= rows; i++) {
-      let x = '';
+      for (let i = 1; i <= rows; i++) {
+        let x = '';
 
-      res.forEach((col) => {
-        const val = fakeRow[col.column_name];
-        x += `'${val}',`;
-      });
+        res.forEach((col) => {
+          const val = fakeRow[col.column_name];
+          x += `'${val}',`;
+        });
 
-      if (x.endsWith(',')) {
-        x = x.slice(0, -1);
+        if (x.endsWith(',')) {
+          x = x.slice(0, -1);
+        }
+
+        t += `(${x}),`;
       }
 
-      t += `(${x}),`;
+      if (t.endsWith(',')) {
+        t = t.slice(0, -1);
+      }
+
+      await this.dataSource.query(`INSERT INTO ${_tableName} VALUES ${t}`);
+
+      return {
+        message: 'success',
+      };
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.UNPROCESSABLE_ENTITY);
     }
-
-    if (t.endsWith(',')) {
-      t = t.slice(0, -1);
-    }
-
-    await this.dataSource.query(`INSERT INTO ${_tableName} VALUES ${t}`);
-
-    return {
-      message: 'success',
-      insertQuery: `INSERT INTO ${_tableName} VALUES ${t};`,
-    };
   }
 
   async findAll(userId: string) {
-    const q = `SELECT table_name, table_schema FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME LIKE '%${userId}'`;
-    const res = await this.dataSource.query(q);
+    try {
+      const q = `SELECT table_name, table_schema FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME LIKE '%${userId}'`;
+      const res = await this.dataSource.query(q);
 
-    return {
-      message: 'success',
-      q,
-      res,
-    };
+      return {
+        message: 'success',
+        q,
+        res,
+      };
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.UNPROCESSABLE_ENTITY);
+    }
   }
 
   async describe(tableName: string) {
-    const q = `
-    SELECT column_name, data_type, column_default, is_nullable
-    FROM information_schema.columns
-    WHERE table_name = '${tableName}';
-  `;
-    const res = await this.dataSource.query(q);
+    try {
+      const q = `
+      SELECT column_name, data_type, column_default, is_nullable
+      FROM information_schema.columns
+      WHERE table_name = '${tableName}';
+      `;
+      const res = await this.dataSource.query(q);
 
-    return {
-      message: 'success',
-      res,
-    };
+      return {
+        message: 'success',
+        res,
+      };
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.UNPROCESSABLE_ENTITY);
+    }
   }
 
   async executeQuery(userId: string, query: string) {
-    const q = query.replaceAll('<', '').replaceAll('>', `_${userId}`);
-    const res = await this.dataSource.query(q);
-    return {
-      message: 'success',
-      res,
-      q,
-    };
+    try {
+      const q = query.replaceAll('<', '').replaceAll('>', `_${userId}`);
+      const res = await this.dataSource.query(q);
+      return {
+        message: 'success',
+        res,
+        q,
+      };
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.UNPROCESSABLE_ENTITY);
+    }
   }
 }
